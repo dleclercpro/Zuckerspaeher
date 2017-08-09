87bba02ab76314469fb604a93147d6766a71e5ec
mm22dl <david.leclerc@tu-dresden.de>
2017-08-10 00:35:56 +0200

    Part of Graph.js according to new ES2015 syntax.

--- dev/modules/graph/graph.js
+++ dev/modules/graph/graph.js
@@ -7,7 +7,9 @@ class Axis {
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
      CONSTRUCTOR
      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+     constructor() {
 
+     }
 
 }
 
@@ -17,6 +19,8 @@ export class Graph {
      CONSTRUCTOR
      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     constructor(name) {
+
+        // Define graph name
         this.name = name;
     }
 
@@ -25,6 +29,18 @@ export class Graph {
      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     generateAxis(z0, dz, dZ) {
 
+        // Initialize empty array
+        let z = [];
+
+        // Generate axis tick
+        for (i = 0; i < (dZ / dz); i++) {
+            z.unshift(z0 - i * dz);
+        }
+
+        // Add last tick based on given dZ
+        z.unshift(z0 - dZ);
+
+        return z;
     }
 
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -32,6 +48,98 @@ export class Graph {
      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     buildAxis(z, z0, dz, dZ, label, format) {
 
+        // Create axis node
+        let axis = $("<div class='graph-" + label + "-axis'></div>");
+
+        // Build axis based on z0, dz and dZ
+        if (z) {
+
+            // Generate axis
+            z = this.generateAxis(z0, dz, dZ);
+
+            for (i = 0; i < z.length - 1; i++) {
+
+                // Compute delta
+                dz = z[i + 1] - z[i];
+
+                // Generate tick
+                const tick = $("<div class='graph-" + label + "-axis-tick'>" +
+                               z[i + 1] + "</div>");
+
+                // Style it
+                tick.css("width", (dz / dZ * 100) + "%");
+
+                // Append it to axis
+                axis.append(tick);
+            }
+        }
+
+
+
+        // Build axis based on provided z array
+        else {
+
+            // Compute total step
+            dZ = Math.max(...z) - Math.min(...z);
+
+            for (i = 0; i < z.length - 1; i++) {
+
+                // Complete step
+                dz = z[i + 1] - z[i];
+
+                // Generate tick
+                const tick = $("<div class='graph-" + label + "-axis-tick'>" +
+                               z[i] + "</div>");
+
+                // Style it
+                tick.css("height", (dz / dZ * 100) + "%");
+
+                // Append it to axis
+                axis.append(tick);
+            }
+        }
+
+        // Format axis ticks if desired
+        if (format) {
+
+            // Convert every tick
+            axis.children().each(() => {
+
+                // Get time
+                let t = $(this).html();
+
+                // Convert time
+                t = lib.convertTime(t, format);
+
+                // Set time
+                $(this).html(t);
+            });
+        }
+
+        // If x-axis
+        if (label == "x") {
+
+            // Store infos on axis
+            this.x = z;
+            this.dX = dZ;
+            this.xMin = Math.min(...z);
+            this.xMax = Math.max(...z);
+
+            // Add dead corner
+            this.buildCorner();
+        }
+        // If y-axis
+        else if (label == "y") {
+            
+            // Store infos on axis
+            this.y = z;
+            this.dY = dZ;
+            this.yMin = Math.min(...z);
+            this.yMax = Math.max(...z);
+        }
+
+        // Append axis to graph
+        this.self.append(axis);
     }
 
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -39,6 +147,8 @@ export class Graph {
      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     buildCorner() {
 
+        // Append to graph
+        this.self.append($("<div class='graph-NA'></div>"));
     }
 
     /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
